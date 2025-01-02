2816139bcad50c212a8de664a856865e67e691b9
daolam <daolavi@gmail.com>
2025-01-02 16:17:18 +0000

    Update contract

--- src/NotifyService.Api/Requests/SendGridEvent.cs
+++ src/NotifyService.Api/Requests/SendGridEvent.cs
@@ -7,8 +7,10 @@ public class SendGridEvent
     [JsonPropertyName("event")]
     public required string EventType { get; set; }
     
+    [JsonPropertyName("email")]
     public required string Email { get; set; }
     
+    [JsonPropertyName("timestamp")]
     public long Timestamp { get; set; }
     
     [JsonPropertyName("custom_args")]
