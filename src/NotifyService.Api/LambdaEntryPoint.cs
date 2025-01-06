eec0917535c442065785ea0ee1fb8c738e66b5e3
daolam <daolavi@gmail.com>
2025-01-06 14:09:13 +0000

    Update

--- src/NotifyService.Api/LambdaEntryPoint.cs
+++ src/NotifyService.Api/LambdaEntryPoint.cs
@@ -1,50 +1,18 @@
-using System.Text.Json;
 using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.AspNetCoreServer;
 using Amazon.Lambda.Core;
-using Amazon.Lambda.Serialization.SystemTextJson;
-using Amazon.Lambda.SQSEvents;
 
 namespace NotifyService.Api;
 
-/// <summary>
-/// This class extends from APIGatewayProxyFunction which contains the method FunctionHandlerAsync which is the 
-/// actual Lambda function entry point. The Lambda handler field should be set to
-/// 
-/// NotifyService.Api::NotifyService.Api.LambdaEntryPoint::FunctionHandlerAsync
-/// </summary>
-public class LambdaEntryPoint :
-
-    // The base class must be set to match the AWS service invoking the Lambda function. If not Amazon.Lambda.AspNetCoreServer
-    // will fail to convert the incoming request correctly into a valid ASP.NET Core request.
-    //
-    // API Gateway REST API                         -> Amazon.Lambda.AspNetCoreServer.APIGatewayProxyFunction
-    // API Gateway HTTP API payload version 1.0     -> Amazon.Lambda.AspNetCoreServer.APIGatewayProxyFunction
-    // API Gateway HTTP API payload version 2.0     -> Amazon.Lambda.AspNetCoreServer.APIGatewayHttpApiV2ProxyFunction
-    // Application Load Balancer                    -> Amazon.Lambda.AspNetCoreServer.ApplicationLoadBalancerFunction
-    // 
-    // Note: When using the AWS::Serverless::Function resource with an event type of "HttpApi" then payload version 2.0
-    // will be the default and you must make Amazon.Lambda.AspNetCoreServer.APIGatewayHttpApiV2ProxyFunction the base class.
-    Amazon.Lambda.AspNetCoreServer.APIGatewayProxyFunction
+public class LambdaEntryPoint : APIGatewayProxyFunction
 {
     private IServiceProvider _serviceProvider;
     
-    /// <summary>
-    /// The builder has configuration, logging and Amazon API Gateway already configured. The startup class
-    /// needs to be configured in this method using the UseStartup<>() method.
-    /// </summary>
-    /// <param name="builder">The IWebHostBuilder to configure.</param>
     protected override void Init(IWebHostBuilder builder)
     {
         builder.UseStartup<Startup>();
     }
 
-    /// <summary>
-    /// Use this override to customize the services registered with the IHostBuilder. 
-    /// 
-    /// It is recommended not to call ConfigureWebHostDefaults to configure the IWebHostBuilder inside this method.
-    /// Instead customize the IWebHostBuilder in the Init(IWebHostBuilder) overload.
-    /// </summary>
-    /// <param name="builder">The IHostBuilder to configure.</param>
     protected override void Init(IHostBuilder builder)
     {
         builder.ConfigureAppConfiguration((_, configurationBuilder) =>
@@ -59,18 +27,11 @@ public class LambdaEntryPoint :
         });
     }
     
-    [LambdaSerializer(typeof(DefaultLambdaJsonSerializer))]
-    public async Task FunctionHandlerAsync(APIGatewayProxyRequest input, ILambdaContext context)
+    public override async Task<APIGatewayProxyResponse> FunctionHandlerAsync(APIGatewayProxyRequest request,
+        ILambdaContext context)
     {
         var logger = _serviceProvider.GetService<ILogger<LambdaEntryPoint>>();
-        if (input != null)
-        {
-            logger.LogInformation("LambdaEntryPoint: {Input}", input);
-            await base.FunctionHandlerAsync(input, context);
-        }
-        else
-        {
-            logger.LogInformation("Processing SQS message");
-        }
+        logger.LogInformation("LambdaEntryPoint: {Request}", request);
+        return await base.FunctionHandlerAsync(request, context);
     }
 }
