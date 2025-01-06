7c0ef2f0f4c71a8fcb607dfe98c11814f2440428
daolam <daolavi@gmail.com>
2025-01-06 14:31:41 +0000

    Update

--- src/NotifyService.Api/LambdaEntryPoint.cs
+++ src/NotifyService.Api/LambdaEntryPoint.cs
@@ -1,6 +1,7 @@
 using Amazon.Lambda.APIGatewayEvents;
 using Amazon.Lambda.AspNetCoreServer;
 using Amazon.Lambda.Core;
+using Amazon.Lambda.SQSEvents;
 
 namespace NotifyService.Api;
 
@@ -33,5 +34,37 @@ public class LambdaEntryPoint : APIGatewayProxyFunction
         var logger = _serviceProvider.GetService<ILogger<LambdaEntryPoint>>();
         logger.LogInformation("LambdaEntryPoint: {Request}", request);
         return await base.FunctionHandlerAsync(request, context);
+    }
+    
+    public void HandleSQSEventAsync(SQSEvent sqsEvent, ILambdaContext context)
+    {
+        var logger = _serviceProvider.GetService<ILogger<LambdaEntryPoint>>();
+
+        foreach (var record in sqsEvent.Records)
+        {
+            logger.LogInformation("Record: {Record}", record);
+        }
+    }
+}
+
+public class LambdaFunction
+{
+    private readonly LambdaEntryPoint _entryPoint = new LambdaEntryPoint();
+
+    public async Task FunctionHandlerAsync(object input, ILambdaContext context)
+    {
+        switch (input)
+        {
+            case APIGatewayProxyRequest apiGatewayRequest:
+                await _entryPoint.FunctionHandlerAsync(apiGatewayRequest, context);
+                break;
+
+            case SQSEvent sqsEvent: 
+                _entryPoint.HandleSQSEventAsync(sqsEvent, context);
+                break;
+
+            default:
+                throw new InvalidOperationException("Unsupported event type");
+        }
     }
 }
